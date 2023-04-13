634567465d6c3d98ce9e974074247f19b40f9d2d
Aoyamaxx <tomako.maketo@gmail.com>
2023-04-13 23:08:00 +0200

    Finished

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # DE-Informative-Website
 
 Building and informative website using HTML and CSS, data processing done by Python.
+
+The purpose of this website is to visualize the data by analyzing the public database of Annual Surface Temperature Change created by climatedata.imf.org to present and bring attention to the current problem of declining groundwater levels in Africa due to rising temperatures.
+
+The website will visualize surface temperature trends globally and in 224 countries specifically, using the data to communicate the facts of global warming. At the same time, the website also disseminates BluePump as a potential temporary solution for Africa water problem.
--- data_analysis.py
+++ data_analysis.py
@@ -4,15 +4,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+# Load data
 df_data = pd.read_csv('data/Annual_Surface_Temperature_Change.csv', na_values = ['-99', ''])
 
-# repalce column name
+# Repalce column name
 new_columns = {str(i): str(i) if i < 10 else str(i + 1951) for i in range(df_data.shape[1])}
 
-# drop some columns with useless info
+# Drop some columns with useless info
 df_data = df_data.drop(df_data.columns[4:10], axis = 1)
 
-# rename the columns of the dataframe
+# Rename the columns of the dataframe
 df_data = df_data.rename(columns=new_columns)
 
 # Reshape the data
@@ -39,9 +40,12 @@ sns.lineplot(data=data_melted, x='Year', y='TemperatureChange', hue='Country', a
 # Plot the linear regression line for the average temperature change
 sns.regplot(data=average_temperature_change, x='Year', y='TemperatureChange', color='black', label='Global Trend', ci=None, order=1, scatter_kws={'zorder': 2}, line_kws={'zorder': 2})
 
-# Calculate the temperature difference
-temp_change = average_temperature_change.loc[average_temperature_change['Year'] == 2021, 'TemperatureChange'].values[0] - \
-              average_temperature_change.loc[average_temperature_change['Year'] == 1961, 'TemperatureChange'].values[0]
+# Calculate the temperature difference using the nearest available years
+nearest_1961_index = average_temperature_change[average_temperature_change['Year'] >= 1961]['TemperatureChange'].notna().idxmax()
+nearest_2021_index = average_temperature_change[average_temperature_change['Year'] <= 2021]['TemperatureChange'].notna().idxmin()
+
+temp_change = average_temperature_change.loc[nearest_2021_index, 'TemperatureChange'] - \
+              average_temperature_change.loc[nearest_1961_index, 'TemperatureChange']
 
 # Display the average temperature change in the right upper corner
 plt.annotate(f"1961: {average_temperature_change.loc[average_temperature_change['Year'] == 1961, 'TemperatureChange'].values[0]:.2f}°C\n"
@@ -52,11 +56,10 @@ plt.annotate(f"1961: {average_temperature_change.loc[average_temperature_change[
 plt.title('Global Surface Temperature Change (1961-2021, in 224 Countries)')
 plt.xlabel('Year')
 plt.ylabel('Temperature Change')
-
-# Get the legend handles and labels
-handles, labels = plt.gca().get_legend_handles_labels()
+plt.axhline(0, color='gray', linestyle='--', zorder=0)
 
 # Remove the legend from the main plot
+handles, labels = plt.gca().get_legend_handles_labels()
 plt.gca().get_legend().remove()
 
 # Save the figure as an image
@@ -71,26 +74,28 @@ ax.axis('off')
 # Save the separate legend as an image
 fig_legend.savefig('country_legend.png', dpi=300, bbox_inches='tight')
 
-
 # Generate graph for each country
-
 if not os.path.exists('country_graphs'):
     os.makedirs('country_graphs')
 
-# Loop through all unique countries in the dataset
 countries = []
 for country in data_melted['Country'].unique():
 
     # Filter data for the current country
     country_data = data_melted[data_melted['Country'] == country]
 
-    # Calculate the temperature difference for the current country
-    temp_change_country = country_data.loc[country_data['Year'] == 2021, 'TemperatureChange'].values[0] - \
-                          country_data.loc[country_data['Year'] == 1961, 'TemperatureChange'].values[0]
+    nearest_1961_index_country = country_data[country_data['Year'] >= 1961]['TemperatureChange'].dropna().idxmin()
+    nearest_2021_index_country = country_data[country_data['Year'] <= 2021]['TemperatureChange'].dropna().idxmax()
 
-    # Store the temperature values for 1961, 2021, and the change for the current country
-    temp_1961_country = country_data.loc[country_data['Year'] == 1961, 'TemperatureChange'].values[0]
-    temp_2021_country = country_data.loc[country_data['Year'] == 2021, 'TemperatureChange'].values[0]
+    temp_change_country = country_data.loc[nearest_2021_index_country, 'TemperatureChange'] - \
+                      country_data.loc[nearest_1961_index_country, 'TemperatureChange']
+
+    nearest_1961_year_country = country_data.loc[nearest_1961_index_country, 'Year']
+    nearest_2021_year_country = country_data.loc[nearest_2021_index_country, 'Year']
+
+    # Store the temperature values for the nearest available years to 1961 and 2021, and the change for the current country
+    temp_1961_country = country_data.loc[nearest_1961_index_country, 'TemperatureChange']
+    temp_2021_country = country_data.loc[nearest_2021_index_country, 'TemperatureChange']
 
     # Create a separate plot for the current country
     plt.figure(figsize=(12, 6))
@@ -101,12 +106,13 @@ for country in data_melted['Country'].unique():
     plt.title(f'Surface Temperature Change for {country} (1961-2021)')
     plt.xlabel('Year')
     plt.ylabel('Temperature Change')
+    plt.axhline(0, color='gray', linestyle='--', zorder=0)
 
     # Display the average temperature change in the right upper corner
-    plt.annotate(f"1961: {temp_1961_country:.2f}°C\n"
-                 f"2021: {temp_2021_country:.2f}°C\n"
-                 f"Change: {temp_change_country:.2f}°C",
-                 xy=(0.95, 0.95), xycoords='axes fraction', fontsize=12, ha='right', va='top', color='orange')
+    plt.annotate(f"{nearest_1961_year_country}: {temp_1961_country:.2f}°C\n"
+                f"{nearest_2021_year_country}: {temp_2021_country:.2f}°C\n"
+                f"Change: {temp_change_country:.2f}°C",
+                xy=(0.95, 0.95), xycoords='axes fraction', fontsize=12, ha='right', va='top', color='orange')
 
     # Replace special characters in the country name
     sanitized_country_name = country.replace(",", "").replace(".", "").replace(" ", "_")
@@ -116,7 +122,28 @@ for country in data_melted['Country'].unique():
     plt.close()
     
     countries.append({"name": country, "image": f'country_graphs/{sanitized_country_name}_temperature_change.png'})
+    
+# Create a bar plot with 0 as the baseline
+plt.figure(figsize=(15, 6))
+bar_colors = ['#1f77b4' if temp_change < 0 else '#d62728' for temp_change in average_temperature_change['TemperatureChange']]
+sns.barplot(data=average_temperature_change, x='Year', y='TemperatureChange', palette=bar_colors)
 
+# Set title and labels for the plot
+plt.title('Average Surface Temperature Change per Year (1961-2021, 224 Countries)')
+plt.xlabel('Year')
+plt.ylabel('Temperature Change')
+plt.axhline(0, color='gray', linestyle='--', zorder=0)
+
+# Rotate x-axis labels and display labels for every 5 years
+plt.xticks(rotation=45)
+xtick_labels = average_temperature_change['Year'].values
+new_xtick_labels = ['' if i % 5 != 0 else str(year) for i, year in enumerate(xtick_labels)]
+plt.gca().set_xticklabels(new_xtick_labels)
+
+# Save the bar plot as an image
+plt.savefig('average_temperature_change_per_year_barplot.png', dpi=300, bbox_inches='tight')
+
+# Generate codes for country_graph.html
 with open("country_graphs.html", "w") as html_file:
     html_file.write('<html>\n<head>\n<style>\n')
     html_file.write('.graph-container { display: flex; flex-wrap: wrap; }\n')
@@ -140,3 +167,5 @@ with open("country_graphs.html", "w") as html_file:
 
     html_file.write('</div>\n')
     html_file.write('</body>\n</html>')
+    
+print("Task finished.")
